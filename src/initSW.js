d8dc6de110c9072be5b0d3041664040fed12b798
Ilya Kreymer <ikreymer@gmail.com>
2019-06-02 21:14:20 -0700

    support relative sw path

--- src/initSW.js
+++ src/initSW.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using a more restrictive scope.
-  navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function(registration) {
+  navigator.serviceWorker.register(window.location.href + 'sw.js', {scope: window.location.href}).then(function(registration) {
     console.log('Service worker registration succeeded:', registration);
   }, /*catch*/ function(error) {
     console.log('Service worker registration failed:', error);
