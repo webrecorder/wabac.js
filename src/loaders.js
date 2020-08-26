9c36bd9bf0b95daedf9ad8f8b75dfd44dda403b1
Ilya Kreymer <ikreymer@gmail.com>
2020-08-26 09:17:09 -0700

    loaders: parse sourceUrl to get extension, ignoring query/fragment

--- src/loaders.js
+++ src/loaders.js
@@ -342,6 +342,13 @@ class WorkerLoader extends CollectionLoader
         config.sourceUrl = file.sourceUrl;
 
         config.sourceName = file.name || file.sourceUrl;
+
+        // parse to strip out query and fragment
+        try {
+          config.sourceName = new URL(config.sourceName).pathname;
+        } catch (e) {
+
+        }
         config.sourceName = config.sourceName.slice(config.sourceName.lastIndexOf("/") + 1);
 
         config.headers = file.headers;
