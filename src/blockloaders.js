0748f6ce17d048b32e43fe56c6c89f1412733d57
Ilya Kreymer <ikreymer@gmail.com>
2022-06-10 16:45:10 -0700

    fix tests, use globalThis instead of self

--- src/blockloaders.js
+++ src/blockloaders.js
@@ -14,7 +14,7 @@ function createLoader(opts) {
   const scheme = url.split(":", 1)[0];
 
   // if URL has same scheme as current origin, use regular http fetch
-  if (scheme === self.location.protocol.split(":")[0]) {
+  if (globalThis && globalThis.location && scheme === globalThis.location.protocol.split(":")[0]) {
     return new HttpRangeLoader(opts);
   }
 
