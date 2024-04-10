73c410ba29a340d731b5297a635e8f0976c8b812
Ilya Kreymer <ikreymer@gmail.com>
2024-04-09 17:28:37 -0700

    readd build outputs for 2.7.3

--- dist/wombatWorkers.js
+++ dist/wombatWorkers.js
@@ -1,1 +1,1 @@
-/*! wombatWorkers.js (wabac.js 2.17.2) is part of Webrecorder project. Copyright (C) 2020-2024, Webrecorder Software. Licensed under the Affero General Public License v3. */(()=>{"use strict"})();
+/*! wombatWorkers.js (wabac.js 2.17.3) is part of Webrecorder project. Copyright (C) 2020-2024, Webrecorder Software. Licensed under the Affero General Public License v3. */(()=>{"use strict"})();
