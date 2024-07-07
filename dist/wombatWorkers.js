ea139ae8b2ee11e0271bb708d5385311a90a4b44
Ilya Kreymer <ikreymer@users.noreply.github.com>
2024-07-07 14:39:17 -0700

    rewrite origin-agent-cluster header as it causes a crash in AWP (#190)
    if set in the chrome extension, see webrecorder/archiveweb.page#194 and
    https://issues.chromium.org/issues/351564775
    bump version to 2.19.3

--- dist/wombatWorkers.js
+++ dist/wombatWorkers.js
@@ -1,1 +1,1 @@
-/*! wombatWorkers.js (wabac.js 2.19.2) is part of Webrecorder project. Copyright (C) 2020-2024, Webrecorder Software. Licensed under the Affero General Public License v3. */(()=>{"use strict"})();
+/*! wombatWorkers.js (wabac.js 2.19.3) is part of Webrecorder project. Copyright (C) 2020-2024, Webrecorder Software. Licensed under the Affero General Public License v3. */(()=>{"use strict"})();
