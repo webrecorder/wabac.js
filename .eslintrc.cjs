58552f4f8be43149ac4a7816ee96a37e55039543
emma <hi@emma.cafe>
2024-09-03 14:52:10 -0400

    update deps & add prettier & eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2021,
-        "sourceType": "module"
-    },
-    "globals": {
-      "globalThis": false, //not writable
-    },
-    "rules": {
-        "no-restricted-globals": [
-            2,
-            "event", "window"
-        ],
-         "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  globals: {
+    globalThis: false, //not writable
+  },
+  rules: {
+    "no-restricted-globals": [2, "event", "window"],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
