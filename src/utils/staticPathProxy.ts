5457c29b8a0651718166e56207766da5c1a8fbd0
emma <hi@emma.cafe>
2025-03-19 19:41:21 -0400

    reuse existing headers

--- src/utils/staticPathProxy.ts
+++ src/utils/staticPathProxy.ts
@@ -2,7 +2,7 @@ export const staticPathProxy =
   (proxyPrefix: string, fetch: typeof self.fetch = self.fetch) =>
   async (url: string, request: Request) => {
     const method = request.method;
-    const headers = new Headers(request.headers);
+    const headers = request.headers;
     // Because of CORS restrictions, the request cannot be a ReadableStream, so instead we get it as a string.
     // If in the future we need to support streaming, we can revisit this — there may be a way to get it to work.
     const body = await request.arrayBuffer();
